b267af1326af11b671469b3d3d563b758435b415
Serkan Surguvec <Srnortw@gmail.com>
2019-02-08 09:25:10 -0500

    Birthday

--- birthday_program.py
+++ birthday_program.py
@@ -1,5 +1,24 @@
-input('Hello I made this program for learn your age.Ok lets start with enter a key :)')
+input('Hello I made this program for learn your age.Ok lets start with enter a key.')
+
+Months_Name=['Jan','Feb','Mar','May','Apr','Jun','July','Aug','Sep','Oct','Nov','Dec']
+
 birth_of_year=input("please enter your year")
+birth_of_month=input('please enter your month')
+
+for index in range(0,12):
+    if(Months_Name[index]==birth_of_month):
+        x=index
+
 current_year=input('what year is this year')
-age=int(current_year)-int(birth_of_year)
-print("Your age is {}".format(age))
+current_mount=input('what month is this month')
+
+for index in range(0,12):
+    if(Months_Name[index]==current_mount):
+        y=index
+
+month=y-x
+
+year=int(current_year)-int(birth_of_year)
+print("Your year is {} Year".format(year))
+print("Your month is {} Month".format(month))
+
