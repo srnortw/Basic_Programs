2e6ac42f94bb08af5c444665f1bb3979c4b37611
Serkan Surguvec <Srnortw@gmail.com>
2019-02-10 06:33:22 -0500

    Palindrome

---
+++ Palindrome_Demo.py
@@ -0,0 +1,8 @@
+Palindrome=input('Please enter a palindrome')
+Palindrome=Palindrome.lower()
+
+
+if(Palindrome==Palindrome[::-1]):
+    print('Yes that string is palindrome')
+else:
+    print('That string is not a palindrome')
