187af572df95999fcfedd5f2ae290526b8bf365b
Serkan Surguvec <Srnortw@gmail.com>
2019-02-23 19:39:50 +0300

    repair

--- Prime_Numbers_Of_Random number.py
+++ Prime_Numbers_Of_Random number.py
@@ -1,17 +1,16 @@
-
 def Prime_Numbers_Of_Random_Number(min,max):
     import random
     a = random.randint(min,max)
     old_a = a
     c = set()
-    for x in range(a-1,2,-1):
+    for x in range(a-1,1,-1):
         if a%x==0:
             b=a//x
 
 
             gate = True
 
-            for p in range(x-1,2,-1):
+            for p in range(x-1,1,-1):
                 if x%p==0:
                     a=x
                     gate=False
@@ -34,13 +33,18 @@ def Prime_Numbers_Of_Random_Number(min,max):
                 c.add(b)
 
 
+    if len(c)==1:
+        strn="is"
+    else:
+        strn="are"
+
 
     if c==set():
         return print(f"This is a prime number:{old_a}")
     else:
-        return print(f"This is not a prime number:{old_a}.And prime elements of That number is {c}")
+        return print(f"This is not a prime number:{old_a}.And prime elements of That number {strn} {c}")
 
-Prime_Numbers_Of_a_Random_Number(20000,2000000)
+Prime_Numbers_Of_Random_Number(1000,30000)
 
 #Enter max and min value(There's no limit by the way).
 #Function chooses a number between min and max.And shows prime numbers of That Number.
